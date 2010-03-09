a509e48fb50873e463579a70b9523c7aabb2ac8f
Chris Shenton <chris@koansys.com>
2010-03-09 16:05:46 -0500

    add username

--- acct_mgr/radius.py
+++ acct_mgr/radius.py
@@ -9,10 +9,9 @@
 #
 # Author: Chris Shenton <chris@koansys.com>
 
-import logging
+from StringIO import StringIO
 
-#from trac.core import *
-from trac.core import Component
+from trac.core import *
 from trac.config import Option
 
 from api import IPasswordStore
@@ -25,7 +24,7 @@ ATTRIBUTE User-Password 2 string encrypt=1
 class RadiusAuthStore(Component):
     implements(IPasswordStore)  # implements is method of Component
 
-    radius_server   = Option('account-manager', 'radius_secret')
+    radius_server   = Option('account-manager', 'radius_server')
     radius_authport = Option('account-manager', 'radius_authport')
     radius_secret   = Option('account-manager', 'radius_secret')
 
@@ -36,43 +35,53 @@ class RadiusAuthStore(Component):
             from pyrad.client import Client, Timeout
             from pyrad.dictionary import Dictionary
         except ImportError, e:
-            logging.error("RADIUS could not import pyrad, need to install the egg: %s" , e)
+            self.log.error("RADIUS could not import pyrad, need to install the egg: %s" , e)
             return None
+        
+        self.log.info("check_password radius_server=%s radius_authport=%s radius_secret=%s" % (
+            self.radius_server, self.radius_authport, self.radius_secret))
+        self.log.info("check_password username=%s password=%s" % (username, password))
+                      
         # What's the encoding of the incoming username, passwd?
-        #username = username.encode('utf-8')
-        #password = password.encode('utf-8')
-        #radius_secret = radius_secret.encode('utf-8')
+        username_utf8 = username.encode('utf-8')
+        password_utf8 = password.encode('utf-8')
+        try:
+            radius_authport_int = int(self.radius_authport)
+        except ValueError, e:
+            self.log.error("radius_authport must be an integer, typically 1813 or 1645")
+            return None
+        radius_secret_utf8 = self.radius_secret.encode('utf-8')
 
-        client = Client(server=radius_server,
-                        authport=radius_authport,
-                        secret=radius_secret,
+        client = Client(server=self.radius_server,
+                        authport=radius_authport_int,
+                        secret=radius_secret_utf8,
                         dict=Dictionary(StringIO(DICTIONARY)),
                         )
 
         req = client.CreateAuthPacket(code=pyrad.packet.AccessRequest,
-                                      User_Name=username)
-        req["User-Password"] = req.PwCrypt(password)
-        logging.warning("RADIUS authenticate sending packet req=%s" % req)
+                                      User_Name=username_utf8)
+        req["User-Password"] = req.PwCrypt(password_utf8)
+        self.log.warning("RADIUS authenticate sending packet req=%s" % req)
         try:
             reply = client.SendPacket(req)
         except Timeout, e:
-            logging.error("RADIUS Timeout contacting radius_server=% radius_authport=%s: %s" % (
-                    radius_server, radius_authport, e))
+            self.log.error("RADIUS Timeout contacting radius_server=% radius_authport=%s: %s" % (
+                    self.radius_server, self.radius_authport, e))
             return None
         except Exception, e:    # TOO BROAD
-            logging.error("RADIUS Unknown error sending to radius_server=% radius_authport=%s: %s" % (
-                    radius_server, radius_authport, e))
+            self.log.error("RADIUS Unknown error sending to radius_server=% radius_authport=%s: %s" % (
+                    self.radius_server, self.radius_authport, e))
             return None
 
-        logging.warning("RADIUS authenticate check reply.coe=%s" % reply.code)
-        if reply.code == pyrad.pycat.AccessAccept:
-            logging.warning("RADIUS Accept username=%s" % username)
+        self.log.warning("RADIUS authenticate check reply.code=%s" % reply.code)
+        if reply.code == pyrad.packet.AccessAccept:
+            self.log.warning("RADIUS Accept username=%s" % username)
             return True
-        elif reply.code == pyrad.pycat.AccessReject:
-            logging.warning("RADIUS Reject username=%s" % username)
+        elif reply.code == pyrad.packet.AccessReject:
+            self.log.warning("RADIUS Reject username=%s" % username)
             return None
         else:
-            logging.error("RADIUS returned unknown code for username=%s reply.code=%s" % (
+            self.log.error("RADIUS returned unknown code for username=%s reply.code=%s" % (
                     username, reply.code))
         return None
 
