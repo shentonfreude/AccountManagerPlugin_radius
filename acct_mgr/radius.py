2a994e026661a34fb8b01bb7cb04c475a10a0ced
Chris <chris@koansys.com>
2010-03-10 18:21:23 -0500

    Some code cleanup around utf-8 mysteries, some docs on how to configure, and handle RADIUS Challenge response which RSA uses for Next Token mode

--- acct_mgr/radius.py
+++ acct_mgr/radius.py
@@ -3,9 +3,9 @@
 # Copyright (C) 2010 Chris Shenton <chris@koansys.com>
 #
 # "THE BEER-WARE LICENSE" (Revision 42):
-# <trac@matt-good.net> wrote this file.  As long as you retain this notice you
+# <chris@koansys.com> wrote this file.  As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
-# this stuff is worth it, you can buy me a beer in return.   Matthew Good
+# this stuff is worth it, you can buy me a beer in return.   
 #
 # Author: Chris Shenton <chris@koansys.com>
 
@@ -13,6 +13,7 @@ from StringIO import StringIO
 
 from trac.core import *
 from trac.config import Option
+from trac.config import IntOption
 
 from api import IPasswordStore
 
@@ -22,67 +23,69 @@ ATTRIBUTE User-Password 2 string encrypt=1
 """
 
 class RadiusAuthStore(Component):
-    implements(IPasswordStore)  # implements is method of Component
+    implements(IPasswordStore)  # 'implements' is method of Component
 
-    radius_server   = Option('account-manager', 'radius_server')
-    radius_authport = Option('account-manager', 'radius_authport')
-    radius_secret   = Option('account-manager', 'radius_secret')
+    radius_server   =    Option('account-manager', 'radius_server')
+    radius_authport = IntOption('account-manager', 'radius_authport')
+    radius_secret   =    Option('account-manager', 'radius_secret')
 
     def check_password(self, username, password):
-        # Do import inside method so we can return 'None' on error
+        # do import inside method so we can fail (return None) if lacking pyrad
         try:
             import pyrad.packet
             from pyrad.client import Client, Timeout
             from pyrad.dictionary import Dictionary
         except ImportError, e:
-            self.log.error("RADIUS could not import pyrad, need to install the egg: %s" , e)
+            self.log.error("RADIUS could not import pyrad," +
+                           " need to install the egg: %s" , e)
             return None
         
-        self.log.info("check_password radius_server=%s radius_authport=%s radius_secret=%s" % (
+        self.log.debug("server=%s authport=%s secret=%s" % (
             self.radius_server, self.radius_authport, self.radius_secret))
-        self.log.info("check_password username=%s password=%s" % (username, password))
+        self.log.debug("username=%s password=%s" % (username, password))
                       
-        # What's the encoding of the incoming username, passwd?
         username_utf8 = username.encode('utf-8')
-        password_utf8 = password.encode('utf-8')
-        try:
-            radius_authport_int = int(self.radius_authport)
-        except ValueError, e:
-            self.log.error("radius_authport must be an integer, typically 1813 or 1645")
-            return None
         radius_secret_utf8 = self.radius_secret.encode('utf-8')
 
         client = Client(server=self.radius_server,
-                        authport=radius_authport_int,
+                        authport=self.radius_authport,
                         secret=radius_secret_utf8,
                         dict=Dictionary(StringIO(DICTIONARY)),
                         )
 
         req = client.CreateAuthPacket(code=pyrad.packet.AccessRequest,
                                       User_Name=username_utf8)
-        req["User-Password"] = req.PwCrypt(password_utf8)
-        self.log.warning("RADIUS authenticate sending packet req=%s" % req)
+        req["User-Password"] = req.PwCrypt(password)
+
+        self.log.debug("RADIUS authenticate sending packet req=%s" % req)
         try:
             reply = client.SendPacket(req)
         except Timeout, e:
-            self.log.error("RADIUS Timeout contacting radius_server=% radius_authport=%s: %s" % (
+            self.log.error("Timeout contacting server=% authport=%s: %s" % (
                     self.radius_server, self.radius_authport, e))
             return None
         except Exception, e:    # TOO BROAD
-            self.log.error("RADIUS Unknown error sending to radius_server=% radius_authport=%s: %s" % (
+            self.log.error("Error sending to server=% authport=%s: %s" % (
                     self.radius_server, self.radius_authport, e))
             return None
+        self.log.debug("RADIUS authenticate check reply.code=%s" % reply.code)
 
-        self.log.warning("RADIUS authenticate check reply.code=%s" % reply.code)
         if reply.code == pyrad.packet.AccessAccept:
-            self.log.warning("RADIUS Accept username=%s" % username)
+            self.log.debug("RADIUS Accept username=%s" % username)
             return True
         elif reply.code == pyrad.packet.AccessReject:
             self.log.warning("RADIUS Reject username=%s" % username)
             return None
+        # Is there any way to alert the user their RSA token is in
+        # Next Token mode so they know to fix it?
+        elif reply.code == pyrad.packet.AccessChallenge:
+            self.log.warning("RADIUS returned Challenge; on RSA servers this" +
+                             "indicates Next Token mode. " +
+                             "username=%s" % username)
+            return None
         else:
-            self.log.error("RADIUS returned unknown code for username=%s reply.code=%s" % (
-                    username, reply.code))
+            self.log.error("Unknown READIUS reply code for username=%s " +
+                           " reply.code=%s" % (username, reply.code))
         return None
 
     def get_users(self):
